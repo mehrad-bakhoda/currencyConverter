70627ab197734961c3583f7f8594defebf69bfba
BlueShdw <no2.nemesis@gmail.com>
2022-06-11 19:10:23 +0430

    free currency converted api added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import superjson from "superjson";
 
-export default function Home() {
-  const [amount, setAmount] = useState(0);
+export default function Home({ data }) {
+  const [amount, setAmount] = useState(20);
   const [currency, setCurrency] = useState("");
   const [targetCurrency, setTargetCurrency] = useState("");
 
@@ -11,14 +12,23 @@ export default function Home() {
   };
   const handleTargetCurrency = (e) => {
     setTargetCurrency(e.target.value);
+
+    var myHeaders = new Headers();
+    myHeaders.append("apikey", "HxjEX0NL6JlbRnPXMnKeIkgL4jFMw1sS");
+
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+      headers: myHeaders,
+    };
+
     fetch(
-      `https://api.currconv.com/api/v7/convert?q=${currency}_${targetCurrency}&compact=ultra&apiKey=b5939a986a2b839e0797`,
-      {
-        method: "POST",
-      }
-    ).then((response) => {
-      console.log(response);
-    });
+      `https://api.apilayer.com/fixer/convert?to=${targetCurrency}&from=${currency}&amount=${amount}`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   };
   return (
     <div className={styles.main}>
@@ -29,174 +39,45 @@ export default function Home() {
         <div className={styles.wrapper}>
           <input />
           <select name="currency" onChange={handleCurrency}>
-            <option value="USD">United States Dollars</option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">United Kingdom Pounds</option>
-            <option value="DZD">Algeria Dinars</option>
-            <option value="ARP">Argentina Pesos</option>
-            <option value="AUD">Australia Dollars</option>
-            <option value="ATS">Austria Schillings</option>
-            <option value="BSD">Bahamas Dollars</option>
-            <option value="BBD">Barbados Dollars</option>
-            <option value="BEF">Belgium Francs</option>
-            <option value="BMD">Bermuda Dollars</option>
-            <option value="BRR">Brazil Real</option>
-            <option value="BGL">Bulgaria Lev</option>
-            <option value="CAD">Canada Dollars</option>
-            <option value="CLP">Chile Pesos</option>
-            <option value="CNY">China Yuan Renmimbi</option>
-            <option value="CYP">Cyprus Pounds</option>
-            <option value="CSK">Czech Republic Koruna</option>
-            <option value="DKK">Denmark Kroner</option>
-            <option value="NLG">Dutch Guilders</option>
-            <option value="XCD">Eastern Caribbean Dollars</option>
-            <option value="EGP">Egypt Pounds</option>
-            <option value="FJD">Fiji Dollars</option>
-            <option value="FIM">Finland Markka</option>
-            <option value="FRF">France Francs</option>
-            <option value="DEM">Germany Deutsche Marks</option>
-            <option value="XAU">Gold Ounces</option>
-            <option value="GRD">Greece Drachmas</option>
-            <option value="HKD">Hong Kong Dollars</option>
-            <option value="HUF">Hungary Forint</option>
-            <option value="ISK">Iceland Krona</option>
-            <option value="INR">India Rupees</option>
-            <option value="IDR">Indonesia Rupiah</option>
-            <option value="IEP">Ireland Punt</option>
-            <option value="ILS">Israel New Shekels</option>
-            <option value="ITL">Italy Lira</option>
-            <option value="JMD">Jamaica Dollars</option>
-            <option value="JPY">Japan Yen</option>
-            <option value="JOD">Jordan Dinar</option>
-            <option value="KRW">Korea (South) Won</option>
-            <option value="LBP">Lebanon Pounds</option>
-            <option value="LUF">Luxembourg Francs</option>
-            <option value="MYR">Malaysia Ringgit</option>
-            <option value="MXP">Mexico Pesos</option>
-            <option value="NLG">Netherlands Guilders</option>
-            <option value="NZD">New Zealand Dollars</option>
-            <option value="NOK">Norway Kroner</option>
-            <option value="PKR">Pakistan Rupees</option>
-            <option value="XPD">Palladium Ounces</option>
-            <option value="PHP">Philippines Pesos</option>
-            <option value="XPT">Platinum Ounces</option>
-            <option value="PLZ">Poland Zloty</option>
-            <option value="PTE">Portugal Escudo</option>
-            <option value="ROL">Romania Leu</option>
-            <option value="RUR">Russia Rubles</option>
-            <option value="SAR">Saudi Arabia Riyal</option>
-            <option value="XAG">Silver Ounces</option>
-            <option value="SGD">Singapore Dollars</option>
-            <option value="SKK">Slovakia Koruna</option>
-            <option value="ZAR">South Africa Rand</option>
-            <option value="KRW">South Korea Won</option>
-            <option value="ESP">Spain Pesetas</option>
-            <option value="XDR">Special Drawing Right (IMF)</option>
-            <option value="SDD">Sudan Dinar</option>
-            <option value="SEK">Sweden Krona</option>
-            <option value="CHF">Switzerland Francs</option>
-            <option value="TWD">Taiwan Dollars</option>
-            <option value="THB">Thailand Baht</option>
-            <option value="TTD">Trinidad and Tobago Dollars</option>
-            <option value="TRL">Turkey Lira</option>
-            <option value="VEB">Venezuela Bolivar</option>
-            <option value="ZMK">Zambia Kwacha</option>
-            <option value="EUR">Euro</option>
-            <option value="XCD">Eastern Caribbean Dollars</option>
-            <option value="XDR">Special Drawing Right (IMF)</option>
-            <option value="XAG">Silver Ounces</option>
-            <option value="XAU">Gold Ounces</option>
-            <option value="XPD">Palladium Ounces</option>
-            <option value="XPT">Platinum Ounces</option>
+            {data.symbols.map((symbol) => {
+              <option value={`${symbol}`}>United States Dollars</option>;
+              console.log(symbol);
+            })}
           </select>
         </div>
         <div className={styles.wrapper}>
           <input className={styles.converted} />
           <select name="targetCurrency" onChange={handleTargetCurrency}>
-            <option value="USD" selected="selected">
-              United States Dollars
-            </option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">United Kingdom Pounds</option>
-            <option value="DZD">Algeria Dinars</option>
-            <option value="ARP">Argentina Pesos</option>
-            <option value="AUD">Australia Dollars</option>
-            <option value="ATS">Austria Schillings</option>
-            <option value="BSD">Bahamas Dollars</option>
-            <option value="BBD">Barbados Dollars</option>
-            <option value="BEF">Belgium Francs</option>
-            <option value="BMD">Bermuda Dollars</option>
-            <option value="BRR">Brazil Real</option>
-            <option value="BGL">Bulgaria Lev</option>
-            <option value="CAD">Canada Dollars</option>
-            <option value="CLP">Chile Pesos</option>
-            <option value="CNY">China Yuan Renmimbi</option>
-            <option value="CYP">Cyprus Pounds</option>
-            <option value="CSK">Czech Republic Koruna</option>
-            <option value="DKK">Denmark Kroner</option>
-            <option value="NLG">Dutch Guilders</option>
-            <option value="XCD">Eastern Caribbean Dollars</option>
-            <option value="EGP">Egypt Pounds</option>
-            <option value="FJD">Fiji Dollars</option>
-            <option value="FIM">Finland Markka</option>
-            <option value="FRF">France Francs</option>
-            <option value="DEM">Germany Deutsche Marks</option>
-            <option value="XAU">Gold Ounces</option>
-            <option value="GRD">Greece Drachmas</option>
-            <option value="HKD">Hong Kong Dollars</option>
-            <option value="HUF">Hungary Forint</option>
-            <option value="ISK">Iceland Krona</option>
-            <option value="INR">India Rupees</option>
-            <option value="IDR">Indonesia Rupiah</option>
-            <option value="IEP">Ireland Punt</option>
-            <option value="ILS">Israel New Shekels</option>
-            <option value="ITL">Italy Lira</option>
-            <option value="JMD">Jamaica Dollars</option>
-            <option value="JPY">Japan Yen</option>
-            <option value="JOD">Jordan Dinar</option>
-            <option value="KRW">Korea (South) Won</option>
-            <option value="LBP">Lebanon Pounds</option>
-            <option value="LUF">Luxembourg Francs</option>
-            <option value="MYR">Malaysia Ringgit</option>
-            <option value="MXP">Mexico Pesos</option>
-            <option value="NLG">Netherlands Guilders</option>
-            <option value="NZD">New Zealand Dollars</option>
-            <option value="NOK">Norway Kroner</option>
-            <option value="PKR">Pakistan Rupees</option>
-            <option value="XPD">Palladium Ounces</option>
-            <option value="PHP">Philippines Pesos</option>
-            <option value="XPT">Platinum Ounces</option>
-            <option value="PLZ">Poland Zloty</option>
-            <option value="PTE">Portugal Escudo</option>
-            <option value="ROL">Romania Leu</option>
-            <option value="RUR">Russia Rubles</option>
-            <option value="SAR">Saudi Arabia Riyal</option>
-            <option value="XAG">Silver Ounces</option>
-            <option value="SGD">Singapore Dollars</option>
-            <option value="SKK">Slovakia Koruna</option>
-            <option value="ZAR">South Africa Rand</option>
-            <option value="KRW">South Korea Won</option>
-            <option value="ESP">Spain Pesetas</option>
-            <option value="XDR">Special Drawing Right (IMF)</option>
-            <option value="SDD">Sudan Dinar</option>
-            <option value="SEK">Sweden Krona</option>
-            <option value="CHF">Switzerland Francs</option>
-            <option value="TWD">Taiwan Dollars</option>
-            <option value="THB">Thailand Baht</option>
-            <option value="TTD">Trinidad and Tobago Dollars</option>
-            <option value="TRL">Turkey Lira</option>
-            <option value="VEB">Venezuela Bolivar</option>
-            <option value="ZMK">Zambia Kwacha</option>
-            <option value="EUR">Euro</option>
-            <option value="XCD">Eastern Caribbean Dollars</option>
-            <option value="XDR">Special Drawing Right (IMF)</option>
-            <option value="XAG">Silver Ounces</option>
-            <option value="XAU">Gold Ounces</option>
-            <option value="XPD">Palladium Ounces</option>
-            <option value="XPT">Platinum Ounces</option>
+            {data.symbols.map((symbol) => {
+              <option value={`${symbol}`}>United States Dollars</option>;
+              console.log(symbol);
+            })}
           </select>
         </div>
       </div>
     </div>
   );
+}
+export async function getServerSideProps() {
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "HxjEX0NL6JlbRnPXMnKeIkgL4jFMw1sS");
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+    headers: myHeaders,
+  };
+  let data = {};
+
+  await fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      data = result;
+    })
+    .catch((error) => console.log("error", error));
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
